f1f905599564c0fa137d4fce400410a2141f4fb0
Rich461123 <sssss514495@gmail.com>
2021-08-02 14:33:31 +0800

    Initial commit

---
+++ app/src/main/java/com/example/shapelist/DetailActivity.java
@@ -0,0 +1,37 @@
+package com.example.shapelist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    Shape selectedShape;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        getSelectedShape();
+        setValue();
+    }
+
+    private void getSelectedShape() {
+        Intent previousIntent = getIntent();
+        String parsedStringID = previousIntent.getStringExtra("id");
+        selectedShape = MainActivity.shapeList.get(Integer.valueOf(parsedStringID));
+    }
+
+    private void setValue() {
+        TextView tv = (TextView) findViewById(R.id.shapeName);
+        ImageView iv = (ImageView) findViewById(R.id.shapeImage);
+
+        tv.setText(selectedShape.getName());
+        iv.setImageResource(selectedShape.getImage());
+    }
+
+}
